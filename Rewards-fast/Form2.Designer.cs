7fbfb66d494a16f065bc9cef7ad60d3cf1569186
Pogulaeva <151159182+Pogulaeva@users.noreply.github.com>
2025-12-16 20:04:32 +0300

    Интерфейс второй формы
    Создание интерфейса второй формы:
    установка полей для выбора изображения, списка ФИО, и расположения создаваемой папки, а также название самой папки
    Пока что нет записи в переменные указанных в полях данных.

--- Rewards-fast/Form2.Designer.cs
+++ Rewards-fast/Form2.Designer.cs
@@ -28,20 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label_explanatory_text2 = new System.Windows.Forms.Label();
+            this.textBox_folder_name = new System.Windows.Forms.TextBox();
+            this.label__folder_name = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button_choosing_location = new System.Windows.Forms.Button();
+            this.label_location = new System.Windows.Forms.Label();
+            this.textBox_location = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox_image_location = new System.Windows.Forms.TextBox();
+            this.label_image = new System.Windows.Forms.Label();
+            this.button_image_location = new System.Windows.Forms.Button();
+            this.textBox_Full_Name_list_location = new System.Windows.Forms.TextBox();
+            this.label_Full_Name_list = new System.Windows.Forms.Label();
+            this.button_Full_Name_list_location = new System.Windows.Forms.Button();
+            this.button_your_template = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label_explanatory_text2
+            // 
+            this.label_explanatory_text2.AutoSize = true;
+            this.label_explanatory_text2.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_explanatory_text2.ForeColor = System.Drawing.Color.White;
+            this.label_explanatory_text2.Location = new System.Drawing.Point(22, 18);
+            this.label_explanatory_text2.Name = "label_explanatory_text2";
+            this.label_explanatory_text2.Size = new System.Drawing.Size(474, 33);
+            this.label_explanatory_text2.TabIndex = 0;
+            this.label_explanatory_text2.Text = "Создание наградного материала";
+            // 
+            // textBox_folder_name
+            // 
+            this.textBox_folder_name.BackColor = System.Drawing.Color.SeaGreen;
+            this.textBox_folder_name.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_folder_name.ForeColor = System.Drawing.Color.White;
+            this.textBox_folder_name.Location = new System.Drawing.Point(28, 263);
+            this.textBox_folder_name.Name = "textBox_folder_name";
+            this.textBox_folder_name.Size = new System.Drawing.Size(794, 22);
+            this.textBox_folder_name.TabIndex = 1;
+            this.textBox_folder_name.Text = "OutputMaterials";
+            // 
+            // label__folder_name
+            // 
+            this.label__folder_name.AutoSize = true;
+            this.label__folder_name.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label__folder_name.ForeColor = System.Drawing.Color.White;
+            this.label__folder_name.Location = new System.Drawing.Point(28, 227);
+            this.label__folder_name.Name = "label__folder_name";
+            this.label__folder_name.Size = new System.Drawing.Size(215, 23);
+            this.label__folder_name.TabIndex = 2;
+            this.label__folder_name.Text = "Папка для сохранения";
+            // 
+            // button_choosing_location
+            // 
+            this.button_choosing_location.BackColor = System.Drawing.Color.Green;
+            this.button_choosing_location.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button_choosing_location.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_choosing_location.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_choosing_location.ForeColor = System.Drawing.Color.Black;
+            this.button_choosing_location.Location = new System.Drawing.Point(828, 337);
+            this.button_choosing_location.Name = "button_choosing_location";
+            this.button_choosing_location.Size = new System.Drawing.Size(37, 29);
+            this.button_choosing_location.TabIndex = 4;
+            this.button_choosing_location.Text = "...";
+            this.button_choosing_location.UseVisualStyleBackColor = false;
+            this.button_choosing_location.Click += new System.EventHandler(this.button_choosing_location_Click);
+            // 
+            // label_location
+            // 
+            this.label_location.AutoSize = true;
+            this.label_location.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_location.ForeColor = System.Drawing.Color.White;
+            this.label_location.Location = new System.Drawing.Point(24, 306);
+            this.label_location.Name = "label_location";
+            this.label_location.Size = new System.Drawing.Size(132, 23);
+            this.label_location.TabIndex = 5;
+            this.label_location.Text = "Расположение";
+            // 
+            // textBox_location
+            // 
+            this.textBox_location.BackColor = System.Drawing.Color.Green;
+            this.textBox_location.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_location.ForeColor = System.Drawing.Color.White;
+            this.textBox_location.Location = new System.Drawing.Point(28, 341);
+            this.textBox_location.Name = "textBox_location";
+            this.textBox_location.Size = new System.Drawing.Size(794, 22);
+            this.textBox_location.TabIndex = 6;
+            this.textBox_location.Text = "С:\\Users";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox_image_location
+            // 
+            this.textBox_image_location.BackColor = System.Drawing.Color.Green;
+            this.textBox_image_location.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_image_location.ForeColor = System.Drawing.Color.White;
+            this.textBox_image_location.Location = new System.Drawing.Point(28, 112);
+            this.textBox_image_location.Name = "textBox_image_location";
+            this.textBox_image_location.Size = new System.Drawing.Size(794, 22);
+            this.textBox_image_location.TabIndex = 9;
+            // 
+            // label_image
+            // 
+            this.label_image.AutoSize = true;
+            this.label_image.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_image.ForeColor = System.Drawing.Color.White;
+            this.label_image.Location = new System.Drawing.Point(24, 76);
+            this.label_image.Name = "label_image";
+            this.label_image.Size = new System.Drawing.Size(122, 23);
+            this.label_image.TabIndex = 8;
+            this.label_image.Text = "Изображение";
+            // 
+            // button_image_location
+            // 
+            this.button_image_location.BackColor = System.Drawing.Color.Green;
+            this.button_image_location.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button_image_location.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_image_location.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_image_location.ForeColor = System.Drawing.Color.Black;
+            this.button_image_location.Location = new System.Drawing.Point(828, 108);
+            this.button_image_location.Name = "button_image_location";
+            this.button_image_location.Size = new System.Drawing.Size(37, 29);
+            this.button_image_location.TabIndex = 7;
+            this.button_image_location.Text = "...";
+            this.button_image_location.UseVisualStyleBackColor = false;
+            this.button_image_location.Click += new System.EventHandler(this.button_image_location_Click);
+            // 
+            // textBox_Full_Name_list_location
+            // 
+            this.textBox_Full_Name_list_location.BackColor = System.Drawing.Color.Green;
+            this.textBox_Full_Name_list_location.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_Full_Name_list_location.ForeColor = System.Drawing.Color.White;
+            this.textBox_Full_Name_list_location.Location = new System.Drawing.Point(28, 188);
+            this.textBox_Full_Name_list_location.Name = "textBox_Full_Name_list_location";
+            this.textBox_Full_Name_list_location.Size = new System.Drawing.Size(794, 22);
+            this.textBox_Full_Name_list_location.TabIndex = 12;
+            // 
+            // label_Full_Name_list
+            // 
+            this.label_Full_Name_list.AutoSize = true;
+            this.label_Full_Name_list.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Full_Name_list.ForeColor = System.Drawing.Color.White;
+            this.label_Full_Name_list.Location = new System.Drawing.Point(24, 152);
+            this.label_Full_Name_list.Name = "label_Full_Name_list";
+            this.label_Full_Name_list.Size = new System.Drawing.Size(220, 23);
+            this.label_Full_Name_list.TabIndex = 11;
+            this.label_Full_Name_list.Text = "Файл со списком ФИО";
+            // 
+            // button_Full_Name_list_location
+            // 
+            this.button_Full_Name_list_location.BackColor = System.Drawing.Color.Green;
+            this.button_Full_Name_list_location.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button_Full_Name_list_location.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Full_Name_list_location.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Full_Name_list_location.ForeColor = System.Drawing.Color.Black;
+            this.button_Full_Name_list_location.Location = new System.Drawing.Point(828, 184);
+            this.button_Full_Name_list_location.Name = "button_Full_Name_list_location";
+            this.button_Full_Name_list_location.Size = new System.Drawing.Size(37, 29);
+            this.button_Full_Name_list_location.TabIndex = 10;
+            this.button_Full_Name_list_location.Text = "...";
+            this.button_Full_Name_list_location.UseVisualStyleBackColor = false;
+            this.button_Full_Name_list_location.Click += new System.EventHandler(this.button_Full_Name_list_location_Click);
+            // 
+            // button_your_template
+            // 
+            this.button_your_template.BackColor = System.Drawing.Color.Green;
+            this.button_your_template.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button_your_template.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_your_template.Font = new System.Drawing.Font("Nirmala UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_your_template.ForeColor = System.Drawing.Color.White;
+            this.button_your_template.Location = new System.Drawing.Point(772, 394);
+            this.button_your_template.Name = "button_your_template";
+            this.button_your_template.Size = new System.Drawing.Size(93, 31);
+            this.button_your_template.TabIndex = 13;
+            this.button_your_template.Text = "Создание";
+            this.button_your_template.UseVisualStyleBackColor = false;
+            this.button_your_template.Click += new System.EventHandler(this.button_your_template_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(664, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 31);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Additional_information
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SeaGreen;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(897, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_your_template);
+            this.Controls.Add(this.textBox_Full_Name_list_location);
+            this.Controls.Add(this.label_Full_Name_list);
+            this.Controls.Add(this.button_Full_Name_list_location);
+            this.Controls.Add(this.textBox_image_location);
+            this.Controls.Add(this.label_image);
+            this.Controls.Add(this.button_image_location);
+            this.Controls.Add(this.textBox_location);
+            this.Controls.Add(this.label_location);
+            this.Controls.Add(this.button_choosing_location);
+            this.Controls.Add(this.label__folder_name);
+            this.Controls.Add(this.textBox_folder_name);
+            this.Controls.Add(this.label_explanatory_text2);
             this.Name = "Additional_information";
             this.ShowIcon = false;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_explanatory_text2;
+        private System.Windows.Forms.TextBox textBox_folder_name;
+        private System.Windows.Forms.Label label__folder_name;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button_choosing_location;
+        private System.Windows.Forms.Label label_location;
+        private System.Windows.Forms.TextBox textBox_location;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox_image_location;
+        private System.Windows.Forms.Label label_image;
+        private System.Windows.Forms.Button button_image_location;
+        private System.Windows.Forms.TextBox textBox_Full_Name_list_location;
+        private System.Windows.Forms.Label label_Full_Name_list;
+        private System.Windows.Forms.Button button_Full_Name_list_location;
+        private System.Windows.Forms.Button button_your_template;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Rewards-fast/Form2.cs
+++ Rewards-fast/Form2.cs
@@ -16,5 +16,44 @@ namespace Rewards_fast
         {
             InitializeComponent();
         }
+
+        private void button_choosing_location_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox_location.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void button_image_location_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Filedialog = new OpenFileDialog();
+            if (Filedialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox_image_location.Text = Filedialog.FileName;
+            }
+        }
+
+        private void button_Full_Name_list_location_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Filedialog = new OpenFileDialog();
+            if (Filedialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox_Full_Name_list_location.Text = Filedialog.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Initial_form newForm = new Initial_form();
+            newForm.Show();
+            this.Hide();
+        }
+
+        private void button_your_template_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
     }
 }
