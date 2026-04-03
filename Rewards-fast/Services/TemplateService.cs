e3a8cef0e9dc6d621ef563d9a800cdee191dbcb5
Pogulaeva <151159182+Pogulaeva@users.noreply.github.com>
2026-04-03 10:40:26 +0300

    Создание класса для сохранения/загрузки шаблонов
    TemplateService - класс с функциями SaveTemplateAsync и LoadTemplateAsync

---
+++ Rewards-fast/Services/TemplateService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Rewards_Fast2._0.Models;
+
+namespace Rewards_Fast2._0.Services
+{
+    public class TemplateService
+    {
+        public async Task SaveTemplateAsync(Template template, string filePath)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(template, options);
+            await File.WriteAllTextAsync(filePath, json);
+        }
+
+        public async Task<Template?> LoadTemplateAsync(string filePath)
+        {
+            string json = await File.ReadAllTextAsync(filePath);
+            return JsonSerializer.Deserialize<Template>(json);
+        }
+    }
+}
